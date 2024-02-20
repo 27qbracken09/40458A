0e23cffa59064df3fc64e05f14542990fdea13b8
27qbracken09 <quinnbracken09@outlook.com>
2024-02-20 07:42:24 -0700

    Initial Commit

---
+++ Auton 73pts/include/auton.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "vex.h"
+
+class Drive;
+
+extern Drive chassis;
+ 
+void no_auton();
+void left_auton();
+void right_auton();
+void mid_auton();
+void last_red_block_auton();
+void second_basket();
+void third_basket();
+void red_block();
+void two_green();
+void test4green();
+
+void plus_auton();
